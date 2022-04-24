a4840804e84ea770cd57caf86949c07661dbe262
Ashutosh Kumar <ashutosh971880@gmail.com>
2022-04-24 19:11:11 +0530

    navbar, routerLink, material icons, project ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 // Angular Form
 import { FormsModule } from '@angular/forms';
@@ -42,6 +44,8 @@ import { FormsModule } from '@angular/forms';
     HttpClientModule,
     MatSnackBarModule,
     MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
